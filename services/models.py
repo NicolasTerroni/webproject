9fe486b97cb70a9f69c0bdf2fd956f1460c6fb72
nicolasterroni <nsterroni@gmail.com>
2021-01-06 01:07:03 -0300

    services app and model

---
+++ services/models.py
@@ -0,0 +1,23 @@
+"""Services models."""
+
+# Django
+from django.db import models
+
+class Service(models.Model):
+    """Services model."""
+    title=models.CharField(max_length=50)
+    content=models.CharField(max_length=50)
+    image=models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    created=models.DateField(auto_now_add=True)
+    updated=models.DateField(auto_now_add=True)
+
+    class Meta:
+        """Service's metadata."""
+        verbose_name = "Service"
+        verbose_name_plural = "Services"
+
+    def __str__(self):
+        return self.title
+
+    """def get_absolute_url(self):
+        return reverse("Service_detail", kwargs={"pk": self.pk})"""

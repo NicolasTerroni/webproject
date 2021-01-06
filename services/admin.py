db68092e92d89f2befc74d6e6a121525c043b908
nicolasterroni <nsterroni@gmail.com>
2021-01-06 04:22:08 -0300

    services in admin dashboard

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,18 @@
+"""Services admin config"""
+
+#Django
 from django.contrib import admin
 
-# Register your models here.
+#Models
+from services.models import Service
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    """Service admin."""
+    date_hierarchy = "created"
+    readonly_fields=('id','created','updated')
+    fields=('id','title','content','image','created','updated')
+    list_display=('id','title','content','image','created','updated')
+    list_display_links=('id','title')
+    list_filter=('created','updated')
+    search_fields=('id','title')
--- services/models.py
+++ services/models.py
@@ -2,12 +2,13 @@
 
 # Django
 from django.db import models
+from django.urls import reverse
 
 class Service(models.Model):
     """Services model."""
     title=models.CharField(max_length=50)
     content=models.CharField(max_length=50)
-    image=models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    image=models.ImageField(upload_to='services', height_field=None, width_field=None, max_length=None)
     created=models.DateField(auto_now_add=True)
     updated=models.DateField(auto_now_add=True)
 
@@ -19,5 +20,5 @@ class Service(models.Model):
     def __str__(self):
         return self.title
 
-    """def get_absolute_url(self):
+"""    def get_absolute_url(self):
         return reverse("Service_detail", kwargs={"pk": self.pk})"""

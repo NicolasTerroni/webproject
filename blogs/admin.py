0072854a81233dab3469543ff9be9103eba493d4
nicolasterroni <nsterroni@gmail.com>
2021-01-06 22:04:33 -0300

    blog and category registered in admin site

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,24 @@
+"""Blog's admin configuration"""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from blogs.models import Category, Blog
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Category admin configuration."""
+
+    readonly_fields=('id','created','updated')
+    fields=('id','name','created','updated')
+    list_display=('id','name','created','updated')
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    """Blog admin configuration."""
+
+    readonly_fields=('id','created','updated')
+    fields=('id','title','content','image','author','category','created','updated')
+    #list_display=('id','title','content','image','author','category','created','updated')

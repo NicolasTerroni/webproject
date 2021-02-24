a975b615a9e97e835c83228ded4cff1a48326cb7
nicolasterroni <nsterroni@gmail.com>
2021-02-24 00:22:16 -0300

    product and productcategory models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,34 @@
+"""Store app models"""
+
+# Django
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    """Product's categories model"""
+    
+    name = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Product category"
+        verbose_name_plural = "Product categories"
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Product(models.Model):
+    """Product's model"""
+
+    name = models.CharField(max_length=100)
+    categories = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="store", null=True, blank=True)
+    price = models.FloatField()
+    aviable = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"

e8bd9255c5c85ad0f0dbdf3f8d913bd86978cc92
nicolasterroni <nsterroni@gmail.com>
2021-02-24 00:40:28 -0300

    store models registered in admin

--- store/admin.py
+++ store/admin.py
@@ -4,3 +4,23 @@
 from django.contrib import admin
 # Models
 from store.models import Product, ProductCategory
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ("id","created_at","updated_at")
+
+    list_display = (    "id",
+                        "name",
+                        "categories",
+                        "image",
+                        "price",
+                        "aviable",
+                        "created_at",
+                        "updated_at")
+
+    search_fields = ("id","name")
+    list_filter = ("categories","aviable","created_at","updated_at")
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ("created","updated")
